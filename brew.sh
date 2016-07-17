55a98844281ea51cf57b61f7a853a2d63e15debb
Benedikt Arnold <ben@benarnold.de>
2016-07-17 22:41:18 +0200

    script to setup a new computer with brew and cask

---
+++ brew.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+brew install ack \
+    zsh \
+    git \
+    curl \
+    maven \
+    ssh-copy-id \
+    youtube-dl
+
+brew cask install google-chrome \
+    adobe-creative-cloud \
+    intellij-idea \
+    1password \
+    arq \
+    visual-studio-code \
+    bittorrent-sync \
+    beamer \
+    docker \
+    dropbox \
+    garmin-express \
+    iterm2 \
+    istat-menus \
+    libreoffice \
+    logitech-myharmony \
+    skype \
+    sonos \
+    viscosity \
+    vlc \
+    yubikey-neo-manager \
+    tower

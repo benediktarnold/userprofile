2a155b692a0e44fd53402ca6253c1ecc2fd468c0
Benedikt Arnold <ben@benarnold.de>
2020-06-04 18:20:16 +0200

    Update brew file before migrating

--- brew.sh
+++ brew.sh
@@ -4,12 +4,7 @@ echo "Installing homebrew"
 
 echo "Intall homebrew formulas"
 brew install ack 
-brew install zsh 
-brew install git 
-brew install curl
-brew install maven
-brew install ssh-copy-id
-brew install youtube-dl
+brew install httpie
 brew install httpie
 brew install ansible
 brew install p7z
@@ -19,26 +14,12 @@ echo "Install some apps with brew cask"
 brew tap caskroom/cask
 brew cask install google-chrome
 brew cask install firefox
-brew cask install adobe-creative-cloud
 brew cask install intellij-idea
-brew cask install 1password
-brew cask install arq
 brew cask install visual-studio-code
-brew cask install bittorrent-sync
-brew cask install beamer
 brew cask install docker
 brew cask install dropbox
-brew cask install garmin-express
 brew cask install iterm2
-brew cask install istat-menus
-brew cask install libreoffice
-brew cask install logitech-myharmony
-brew cask install skype
-brew cask install sonos
-brew cask install viscosity
 brew cask install vlc
-brew cask install yubikey-neo-manager
-brew cask install tower
 
 echo "cleanup"
 brew cask cleanup

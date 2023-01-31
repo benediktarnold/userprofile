7a748d91e84780d9c8322f264927b7dc3f71a2fb
Benedikt Arnold <ben@benarnold.de>
2023-01-31 11:49:17 +0100

    remove some packages

--- brew.sh
+++ brew.sh
@@ -2,7 +2,6 @@
 echo "Intall homebrew formulas"
 brew install ack 
 brew install httpie
-brew install ansible
 brew install p7z
 brew install md5sha1sum
 brew install watch
@@ -11,7 +10,6 @@ brew install jq
 echo "Install some apps with brew cask"
 brew install google-chrome
 brew install firefox
-brew install intellij-idea
 brew install visual-studio-code
 brew install docker
 brew install dropbox
@@ -19,7 +17,6 @@ brew install iterm2
 brew install vlc
 brew install slack
 brew install microsoft-teams
-brew install adoptopenjdk11
 
 echo "cleanup"
 brew cleanup
